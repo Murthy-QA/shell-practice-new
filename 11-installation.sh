01fdb0d6a0de3e1e9f198909318872afea100734
murthy <n4.murthy@gmail.com>
2026-02-10 14:58:17 +0530

    shell practice

--- 11-installation.sh
+++ 11-installation.sh
@@ -4,12 +4,14 @@ USERID=(id -u)
 
 if [ $USERID -ne 0 ];then
     echo "ERROR: Please take root access"
+    ecit 1
 fi
 
 dnf install mysql -y
 
 if [ $? -ne 0 ];then
     echo "Error:Mysql was not installed"
+    exit 1
 else
     echo "Installed Mysql was successful"
 fi

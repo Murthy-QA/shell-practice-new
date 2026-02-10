7b954d15e61b4f33f524ca5aa5d28529d43218ef
murthy <n4.murthy@gmail.com>
2026-02-10 11:16:18 +0530

    shell practice

---
+++ 03-variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+PERSON1="Puja"
+PERSON2="Murthy"
+echo "Hello How are you $PERSON1"
+echo "Hi $PERSON2 I am fine how are you"
+echo "Whats the matter $PERSON1"
+echo "Nothing routine life $PERSON2"

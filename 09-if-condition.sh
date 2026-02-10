5c161bc4810fea056b724b665bc158f7bbba01ab
murthy <n4.murthy@gmail.com>
2026-02-10 12:38:13 +0530

    shell practice

--- 09-if-condition.sh
+++ 09-if-condition.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 Number=$1
-if [ $Number -lt 10 ]; then
-    echo "Number is less than 10"
+if [ $Number -gt 10 ]; then
+    echo "Number is greater than 10"
 else
-    echo "Number is greater than 10" 
+    echo "Number is less than 10" 
 fi

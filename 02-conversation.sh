9ff4debc14053e3b93f8d24543c9d402641bd491
murthy <n4.murthy@gmail.com>
2026-02-10 11:02:34 +0530

    shell practice

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-x = 20
+X = 20
 echo "Hello How are you puja"
 echo "Hi Murthy I am fine how are you"
 echo "Whats the matter"
 echo "Nothing routine life"
-echo "Variable value X is : $x"
+echo "Variable value X is : $X"
